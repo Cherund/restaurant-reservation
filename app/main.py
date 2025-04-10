bcc803484ce8005f9a622fc6e4aa7a83fe6b1632
Egor Gerimovich <cherund@gmail.com>
2025-04-10 14:04:42 +0200

    remove annotations in main.py

--- app/main.py
+++ app/main.py
@@ -4,25 +4,20 @@ from fastapi import FastAPI
 from app.routers import tables, reservations
 from app.db import init_db
 
-# Настройка логгирования
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-# Создание таблиц при запуске
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Действия при старте
     init_db()
     logger.info("Database tables created.")
 
-    yield  # Приложение работает
+    yield
 
-    # Действия при завершении
     logger.info("Shutting down the application.")
 
 
-# Инициализация приложения с lifespan
 app = FastAPI(lifespan=lifespan)
 
 # Роутеры
