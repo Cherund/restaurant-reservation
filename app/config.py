e188cf51e0d221abe78e6fc3a4c63760b734958a
Egor Gerimovich <cherund@gmail.com>
2025-04-10 14:04:05 +0200

    edit DATABASE_URL in config.py

--- app/config.py
+++ app/config.py
@@ -3,4 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:postgres@localhost:5432/restaurant")
+DATABASE_URL = os.getenv("DATABASE_URL",
+                         "postgresql+psycopg2://postgres:postgres@localhost:5432/restaurant")
